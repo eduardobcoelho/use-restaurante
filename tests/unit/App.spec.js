a948f365077f644d5df39cd70aad63edbe72340a
eduardobcoelho <luizbcoelho27@outlook.com>
2022-01-19 09:15:45 -0300

    :test_tube: test: app suite test case

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,32 +1,52 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vue from 'vue';
 import Vuex from 'vuex';
-import Store from '@/store';
+import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import App from '@/App';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
+Vue.use(Vuetify);
 
 describe('App', () => {
   let store;
+  let router;
+
   beforeEach(() => {
     store = new Vuex.Store({
-      ...Store.state,
-      ...Store.mutations,
-      ...Store.actions,
-      ...Store.getters,
+      mutations: {
+        setUser: jest.fn(),
+      },
+      getters: {
+        notification: () => ({
+          type: 'info',
+          message: 'Vocêe está no App.vue',
+        }),
+      },
     });
+    router = new VueRouter();
   });
 
   it('É um component vue', () => {
-    const wrapper = shallowMount(App, { store, localVue });
+    const wrapper = shallowMount(App, { localVue, store, router });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
-  it('Contém todos os elementos necessários', () => {
-    const wrapper = shallowMount(App, { store, localVue });
+  it('Contém os elementos de única instância', () => {
+    const wrapper = shallowMount(App, { localVue, store, router });
     const header = wrapper.findComponent({ name: 'TheHeader' });
     expect(header.exists()).toBe(true);
+    const main = wrapper.findComponent({ name: 'VMain' });
+    expect(main.exists()).toBe(true);
     const footer = wrapper.findComponent({ name: 'TheFooter' });
     expect(footer.exists()).toBe(true);
+  });
+
+  it('Contém o router-view', () => {
+    const wrapper = shallowMount(App, { localVue, store, router });
+    const routerView = wrapper.findComponent({ name: 'RouterView' });
+    expect(routerView.exists()).toBe(true);
   });
 });

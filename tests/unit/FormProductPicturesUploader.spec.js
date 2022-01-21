3070bd946529c234d4e0c5485700739b4e2ff7da
eduardobcoelho <luizbcoelho27@outlook.com>
2022-01-21 17:42:25 -0300

    :test_tube: test: FormProductPicturesUploader some tests on suite case

---
+++ tests/unit/FormProductPicturesUploader.spec.js
@@ -0,0 +1,67 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import FormProductPicturesUploader from '@/views/product/FormProductPicturesUploader';
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+Vue.use(Vuetify);
+
+describe('FormProductPicturesUploader.vue', () => {
+  let router;
+
+  beforeEach(() => {
+    router = new VueRouter();
+  });
+
+  it('Deve mostrar uma tag "Capa" quando isCover for true', () => {
+    const wrapper = mount(FormProductPicturesUploader, {
+      propsData: {
+        reference: 'uploader1',
+        isCover: true,
+      },
+      localVue,
+    });
+    expect(wrapper.find('.uploader__isCover-tag').exists()).toBe(true);
+    expect(wrapper.find('.uploader__isCover-tag').text()).toMatch('Capa');
+  });
+
+  it('O componente deve ser renderizado com o id de acordo com a referência passado como props', () => {
+    const wrapper = mount(FormProductPicturesUploader, {
+      propsData: {
+        reference: 'uploader1',
+      },
+      localVue,
+    });
+
+    expect(wrapper.find(`#${wrapper.vm.$props.reference}`).element.id).toMatch(
+      wrapper.vm.$props.reference,
+    );
+    expect(wrapper.find(`#${wrapper.vm.$props.reference}`).exists()).toBe(true);
+  });
+
+  it('A informação para anexar a imagem só deve aparecer caso a propriedade "image" não seja passada por parâmetro', () => {
+    const wrapper = mount(FormProductPicturesUploader, {
+      propsData: {
+        reference: 'uploader1',
+      },
+      localVue,
+    });
+    expect(wrapper.vm.$props.image).toBeNull();
+    expect(wrapper.find('.uploader__content').exists()).toBe(true);
+  });
+
+  /*it('O método setBackgroundUploader deve ser chamado caso a propriedade "image" tenha um valor válido', () => {
+    const wrapper = mount(FormProductPicturesUploader, {
+      propsData: {
+        reference: 'uploader1',
+        image:
+          'https://static.clubedaanamariabraga.com.br/wp-content/uploads/2019/03/salada-de-polvo-scaled.jpg',
+      },
+      localVue,
+    });
+    jest.spyOn(wrapper.vm, 'setBackgroundUploader');
+    expect(wrapper.vm.setBackgroundUploader).toHaveBeenCalledTimes(1);
+  });*/
+});

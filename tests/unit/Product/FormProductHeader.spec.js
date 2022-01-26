6fbbaa13ebaf7c80fe6c33bb8dc9f470af7b6fae
eduardobcoelho <luizbcoelho27@outlook.com>
2022-01-26 18:05:42 -0300

    refact: FormProductHeader tests clean code

--- tests/unit/Product/FormProductHeader.spec.js
+++ tests/unit/Product/FormProductHeader.spec.js
@@ -6,43 +6,28 @@ import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 
 describe('FormProductHeader.vue', () => {
+  const wrapper = shallowMount(FormProductHeader, {
+    propsData: {
+      step: 1,
+      title: 'Título',
+    },
+  });
+
   it('Mostra o título corretamente', () => {
-    const wrapper = shallowMount(FormProductHeader, {
-      propsData: {
-        step: 1,
-        title: 'Título',
-      },
-    });
     expect(wrapper.text()).toContain('Título');
   });
 
   it('Step 1 mostra "Etapa 1 de 2"', () => {
-    const wrapper = shallowMount(FormProductHeader, {
-      propsData: {
-        step: 1,
-        title: 'Título místico',
-      },
-    });
     expect(wrapper.text()).toContain('Etapa 1 de 2');
   });
 
-  it('Step 2 mostra "Etapa 2 de 2"', () => {
-    const wrapper = shallowMount(FormProductHeader, {
-      propsData: {
-        step: 2,
-        title: 'Título místico',
-      },
-    });
+  it('Step 2 mostra "Etapa 2 de 2"', async () => {
+    await wrapper.setProps({ step: 2 });
     expect(wrapper.text()).toContain('Etapa 2 de 2');
   });
 
   it('Texto informativo da etapa muda de acordo com o Step', async () => {
-    const wrapper = shallowMount(FormProductHeader, {
-      propsData: {
-        step: 1,
-        title: 'Título místico',
-      },
-    });
+    await wrapper.setProps({ step: 1 });
     expect(wrapper.text()).toContain('Etapa 1 de 2');
     await wrapper.setProps({ step: 2 });
     expect(wrapper.text()).toContain('Etapa 2 de 2');
